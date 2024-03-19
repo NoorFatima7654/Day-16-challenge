42479277b6cc149dcaf57fe6297f217884145422
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-19 20:36:27 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+// Question no 46
+let laptop = {
+    make: "Dell",
+    model: "XPS 15",
+    year: "2021",
+    describe: function() {
+        console.log(`This laptop is a ${this.year} ${this.make}  ${this.model}.`);
+    }
+}
+laptop.describe();
+//Qu8estion no 47
+let laptops = [
+    {make: "Dell", model: "XPS 15", year: 2021 },
+    {make: "Apple", model:"MacBook Pro", year : 2020},
+    {make: "HP", model:"Specture x360", year : 2021},  
+];
+let [laptop1, laptop2] = laptops;
+console.log(laptop1);
+console.log(laptop2); 
+//Question no 48
+let pricesSet1 = [1200, 1500,1100];
+let pricesSet2 = [1000, 1300,1600];
+let combinedPrices = [...pricesSet1, ...pricesSet2] .sort((a, b ) =>  a-b);
+console.log(combinedPrices);
